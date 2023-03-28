c785683b93dab03783f8f81ad24151f196e02abb
zzyzhizhi <12110611@mail.sustech.edu.cn>
2023-03-28 20:46:05 +0800

    finish 1-6

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -5,7 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
- *
  * This is just a demo for you, please run it on JDK17 (some statements may be not allowed in lower version).
  * This is just a demo, and you can extend and implement functions
  * based on this demo, or implement it in a different way.
@@ -46,58 +45,57 @@ public class OnlineCoursesAnalyzer {
 
     //1
     public Map<String, Integer> getPtcpCountByInst() {
-        Map<String , Integer> map = new TreeMap<>();
+        Map<String, Integer> map = new TreeMap<>();
         for (int i = 0; i < courses.size(); i++) {
-            map.put(courses.get(i).institution,0);
+            map.put(courses.get(i).institution, 0);
         }
         for (int i = 0; i < courses.size(); i++) {
             int num = courses.get(i).participants;
             String inst = courses.get(i).institution;
             int count = map.get(inst);
             count = count + num;
-            map.put(inst,count);
+            map.put(inst, count);
         }
         return map;
     }
 
     //2
     public Map<String, Integer> getPtcpCountByInstAndSubject() {
-        Map<String , Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < courses.size(); i++) {
             Course temp = courses.get(i);
-            String key = temp.institution+"-"+temp.subject;
-            map.put(key,0);
+            String key = temp.institution + "-" + temp.subject;
+            map.put(key, 0);
         }
         for (int i = 0; i < courses.size(); i++) {
             Course temp = courses.get(i);
             int num = temp.participants;
-            String inst = temp.institution+"-"+temp.subject;
+            String inst = temp.institution + "-" + temp.subject;
             int count = map.get(inst);
             count = count + num;
-            map.put(inst,count);
+            map.put(inst, count);
         }
         List<Map.Entry<String, Integer>> sortList = new ArrayList<>(map.entrySet());
         Collections.sort(sortList, new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 int diff = o2.getValue() - o1.getValue();
-                if (diff==0){
+                if (diff == 0) {
                     return o1.getKey().compareTo(o2.getKey());
-                }
-                else return diff;
+                } else return diff;
             }
         });
-        Map<String ,Integer> sortedmap = new LinkedHashMap<>();
-        for (Map.Entry<String,Integer> entry:sortList
-             ) {
-            sortedmap.put(entry.getKey(),entry.getValue());
+        Map<String, Integer> sortedmap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : sortList
+        ) {
+            sortedmap.put(entry.getKey(), entry.getValue());
         }
         return sortedmap;
     }
 
     //3
     public Map<String, List<List<String>>> getCourseListOfInstructor() {
-        Map<String,List<List<String>>> map = new HashMap<>();
+        Map<String, List<List<String>>> map = new HashMap<>();
         Set<String> instructorsSet = new HashSet<>();
         for (int i = 0; i < courses.size(); i++) {
             Course temp = courses.get(i);
@@ -113,20 +111,19 @@ public class OnlineCoursesAnalyzer {
             List<List<String>> b = new ArrayList<>(2);
             b.add(a);
             b.add(c);
-            map.put(temp,b);
+            map.put(temp, b);
         }
         for (Course temp : courses) {
             String[] str = temp.instructors.split(",");
-            if (str.length==1) {
-                str[0]=str[0].trim();
+            if (str.length == 1) {
+                str[0] = str[0].trim();
                 if (!map.get(str[0]).get(0).contains(temp.title))
-                map.get(str[0]).get(0).add(temp.title);
-            }
-            else {
+                    map.get(str[0]).get(0).add(temp.title);
+            } else {
                 for (int i = 0; i < str.length; i++) {
                     str[i] = str[i].trim();
                     if (!map.get(str[i]).get(1).contains(temp.title))
-                    map.get(str[i]).get(1).add(temp.title);
+                        map.get(str[i]).get(1).add(temp.title);
                 }
             }
         }
@@ -141,25 +138,25 @@ public class OnlineCoursesAnalyzer {
     //4
     public List<String> getCourses(int topK, String by) {
         List<String> list = new ArrayList<>();
-        if (Objects.equals(by, "hours")){
+        if (Objects.equals(by, "hours")) {
             Set<String> title = new HashSet<>();
-            Map<String,Double> map = new HashMap<>();
-            for(Course temp:courses){
+            Map<String, Double> map = new HashMap<>();
+            for (Course temp : courses) {
                 title.add(temp.title);
             }
-            for(String temp : title){
-                map.put(temp,0.0);
+            for (String temp : title) {
+                map.put(temp, 0.0);
             }
-            for(Course temp : courses){
-                if (temp.totalHours>map.get(temp.title))
-                map.put(temp.title,temp.totalHours);
+            for (Course temp : courses) {
+                if (temp.totalHours > map.get(temp.title))
+                    map.put(temp.title, temp.totalHours);
             }
             list.addAll(title);
             Collections.sort(list, new Comparator<String>() {
                 @Override
                 public int compare(String o1, String o2) {
-                    if (map.get(o1)<map.get(o2)) return 1;
-                    if (map.get(o1)>map.get(o2)) return -1;
+                    if (map.get(o1) < map.get(o2)) return 1;
+                    if (map.get(o1) > map.get(o2)) return -1;
                     return o1.compareTo(o2);
                 }
             });
@@ -168,26 +165,25 @@ public class OnlineCoursesAnalyzer {
                 list1.add(list.get(i));
             }
             return list1;
-        }
-        else {
+        } else {
             Set<String> title = new HashSet<>();
-            Map<String,Integer> map = new HashMap<>();
-            for(Course temp:courses){
+            Map<String, Integer> map = new HashMap<>();
+            for (Course temp : courses) {
                 title.add(temp.title);
             }
-            for(String temp : title){
-                map.put(temp,0);
+            for (String temp : title) {
+                map.put(temp, 0);
             }
-            for(Course temp : courses){
-                if (temp.participants>map.get(temp.title))
-                    map.put(temp.title,temp.participants);
+            for (Course temp : courses) {
+                if (temp.participants > map.get(temp.title))
+                    map.put(temp.title, temp.participants);
             }
             list.addAll(title);
             Collections.sort(list, new Comparator<String>() {
                 @Override
                 public int compare(String o1, String o2) {
-                    if (map.get(o1)<map.get(o2)) return 1;
-                    if (map.get(o1)>map.get(o2)) return -1;
+                    if (map.get(o1) < map.get(o2)) return 1;
+                    if (map.get(o1) > map.get(o2)) return -1;
                     return o1.compareTo(o2);
                 }
             });
@@ -202,12 +198,12 @@ public class OnlineCoursesAnalyzer {
     //5
     public List<String> searchCourses(String courseSubject, double percentAudited, double totalCourseHours) {
         courseSubject = courseSubject.toUpperCase(Locale.ROOT);
-        for (Course temp : courses){
+        for (Course temp : courses) {
             temp.subject = temp.subject.toUpperCase(Locale.ROOT);
         }
         Set<String> a = new HashSet<>();
-        for(Course temp : courses){
-            if (temp.subject.contains(courseSubject)&&temp.percentAudited>=percentAudited&&temp.totalHours<=totalCourseHours){
+        for (Course temp : courses) {
+            if (temp.subject.contains(courseSubject) && temp.percentAudited >= percentAudited && temp.totalHours <= totalCourseHours) {
                 a.add(temp.title);
             }
         }
@@ -218,8 +214,83 @@ public class OnlineCoursesAnalyzer {
 
     //6
     public List<String> recommendCourses(int age, int gender, int isBachelorOrHigher) {
+        Set<String> number = new HashSet<>();
+        for (Course temp : courses) {
+            number.add(temp.number);
+        }
+        Map<String, Integer> time = new HashMap<>();
+        Map<String, Double> midage = new HashMap<>();
+        Map<String, Double> midgender = new HashMap<>();
+        Map<String, Double> midisBachelorOrHigher = new HashMap<>();
+        for (String temp : number) {
+            time.put(temp, 0);
+            midage.put(temp, 0.0);
+            midgender.put(temp, 0.0);
+            midisBachelorOrHigher.put(temp, 0.0);
+        }
+        for (Course temp : courses) {
+            int d = time.get(temp.number);
+            d++;
+            time.put(temp.number, d);
+        }
+        for (Course temp : courses) {
+            double a = midage.get(temp.number);
+            a += temp.medianAge;
+            midage.put(temp.number, a);
+            double b = midgender.get(temp.number);
+            b += temp.percentMale;
+            midgender.put(temp.number, b);
+            double c = midisBachelorOrHigher.get(temp.number);
+            c += temp.percentDegree;
+            midisBachelorOrHigher.put(temp.number, c);
+        }
+        for (String temp : number) {
+            double n = midage.get(temp) / time.get(temp);
+            midage.put(temp, n);
+            double m = midgender.get(temp) / time.get(temp);
+            midgender.put(temp, m);
+            double k = midisBachelorOrHigher.get(temp) / time.get(temp);
+            midisBachelorOrHigher.put(temp, k);
+        }
+        Map<String, Double> result = new HashMap<>();
+        for (String temp : number) {
+            double val = Math.pow(age - midage.get(temp), 2) + Math.pow(gender * 100 - midgender.get(temp), 2) + Math.pow(isBachelorOrHigher * 100 - midisBachelorOrHigher.get(temp), 2);
+            result.put(temp, val);
+        }
+        Map<String, List<Course>> numtotitle = new HashMap<>();
+        for (String temp : number) {
+            List<Course> a = new ArrayList<>();
+            numtotitle.put(temp, a);
+        }
+        for (Course temp : courses) {
+            numtotitle.get(temp.number).add(temp);
+        }
+        for (String temp : number) {
+            numtotitle.get(temp).sort(new Comparator<Course>() {
+                @Override
+                public int compare(Course o1, Course o2) {
+                    return o2.launchDate.compareTo(o1.launchDate);
+                }
+            });
+        }
+        Map<String, String> numtitle = new HashMap<>();
+        for (String temp : number) {
+            numtitle.put(temp, numtotitle.get(temp).get(0).title);
+        }
+        List<String> num = new ArrayList<>(number);
+        num.sort((o1, o2) -> {
+            if (result.get(o1) > result.get(o2)) return 1;
+            if (result.get(o1) < result.get(o2)) return -1;
+            return numtitle.get(o1).compareTo(numtitle.get(o2));
+        });
+        Set<String> result2 = new HashSet<>();
+        List<String> result1 = new ArrayList<>();
+        for (String s : num) {
+            if (result2.add(numtitle.get(s)))
+                result1.add(numtitle.get(s));
+        }
 
-        return null;
+        return result1.subList(0,10);
     }
 
 }
